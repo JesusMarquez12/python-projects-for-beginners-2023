a1ac27994c345ae89fe294132b7011818ee33b63
Jesús Márquez <jesus.marquez.ingelo@gmail.com>
2023-08-09 12:06:27 -0400

    Added first iteration of the madlib project

---
+++ madlibs/init.py
@@ -0,0 +1,11 @@
+# This is the first iteration to create an script to emulate a madlib
+# game using fstring
+
+noun = "string"
+adj = "easy"
+verb = "have fun"
+
+paragraph = f"This matlib example consist in put a {noun} into a paragraph in \
+a {adj} way, allowing the user to {verb} in a didactic way."
+
+print(paragraph)
